3d5f81e3803a19fc22bd8626e8eeedb7047f51b8
Kyle.Tryon <Kyle.Tryon@outlook.com>
2021-02-13 17:54:33 -0500

    initial version

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,48 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+export {}
+const script = document.getElementById('teleprompter') as Element
+const startButton = document.getElementById("btnStart")as Element
+const progressElement = document.getElementById("progressStatus")as HTMLInputElement
+let scrollHeight: number;
+let currentPos: number;
+let currentSpeed: number = 2;
+let scrollLoop: number;
+let isStarted: boolean = false;
+//Add Event Listeners
+(document.getElementById("btnFlipX")as Element).addEventListener("click", flipX);
+startButton.addEventListener("click", startScroll);
+script.addEventListener('scroll', updateProgressBar)
 
-import confetti from 'canvas-confetti';
+function flipX(): void {
+  script.classList.toggle('flipX')
+}
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+function startScroll(): void {
+  isStarted = !isStarted
+  startButton.textContent = (isStarted ? "⏸" : "▶")
+  scrollHeight = script.scrollHeight
+  console.log("Height of script: ", scrollHeight)
+  if (isStarted) {
+    scrollLoop = setInterval(()=> {
+      console.log("current position: ", currentPos)
+      currentPos = script.scrollTop
+      scroll(currentPos+currentSpeed)
+      updateProgressBar()
+    },30)
+  } else {
+    clearInterval(scrollLoop)
+  }
+}
+
+function scroll(num: number): void {
+  script.scroll({
+    behavior: "auto",
+    top: num
+  })
+}
+
+function updateProgressBar(): void {
+  currentPos = script.scrollTop
+  scrollHeight = script.scrollHeight
+  const amount = Math.floor(((currentPos + script.clientHeight) / scrollHeight) * 100)
+  progressElement.value = amount.toString()
+}
