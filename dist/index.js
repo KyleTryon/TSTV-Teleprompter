22d264916d40dc6802ef4868c79903f54ac8c95d
KyleTryon <KyleTryon@users.noreply.github.com>
2021-02-14 21:28:35 +0000

    deploy: 4e28d33eaa434f3f48ddc4b97dfb33853e46a748

--- dist/index.js
+++ dist/index.js
@@ -28,7 +28,9 @@ function flipX() {
 }
 function startScroll() {
   isStarted = !isStarted;
-  startButton.textContent = isStarted ? "\u23F8" : "\u25B6";
+  const playIcon = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>';
+  const pauseIcon = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M9 16h2V8H9v8zm3-14C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm1-4h2V8h-2v8z"/></svg>';
+  startButton.innerHTML = isStarted ? pauseIcon : playIcon;
   scrollHeight = script.scrollHeight;
   console.log("Height of script: ", scrollHeight);
   if (isStarted) {
